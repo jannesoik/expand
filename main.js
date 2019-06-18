6cff23b8c5666a05a718f867c74410553afba420
Janne Soikkeli <janne.soikkeli@edu.turkuamk.fi>
2019-06-18 11:33:07 +0300

    expand toimii, teksti puuttuu

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 var leftBox = document.getElementsByClassName('left')[0];
 var rightBox = document.getElementsByClassName('right')[0];
 
+function hideArrow(){
+    leftBox.classList.add("hide");
+}
+
 function expandLeft() {
     rightBox.style.width='90%';
     leftBox.style.width='10%';
+    rightBox.classList.add("hide");
+    leftBox.classList.remove("hide");
 }
 
 function expandRight() {
     leftBox.style.width='90%';
     rightBox.style.width='10%';
+    document.getElementsByClassName('right')[0].style.backgroundImage= "img/vihr-nuol.png";
+    leftBox.classList.add("hide");
+    rightBox.classList.remove("hide");
 }
