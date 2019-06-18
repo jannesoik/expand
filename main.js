fd22fd9efcfa3aee16195f295c95ab325e74a39f
Janne Soikkeli <janne.soikkeli@edu.turkuamk.fi>
2019-06-18 17:16:27 +0300

    kuva ja ots

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var leftBox = document.getElementsByClassName('left')[0];
 var rightBox = document.getElementsByClassName('right')[0];
+var description = document.getElementById('v-text');
+var header = document.getElementById('header');
 
 function hideArrow(){
     leftBox.classList.add("hide");
@@ -10,6 +12,9 @@ function expandLeft() {
     leftBox.style.width='10%';
     rightBox.classList.add("hide");
     leftBox.classList.remove("hide");
+    description.style.visibility='visible';
+    header.style.visibility='hidden';
+
 }
 
 function expandRight() {
@@ -18,4 +23,6 @@ function expandRight() {
     document.getElementsByClassName('right')[0].style.backgroundImage= "img/vihr-nuol.png";
     leftBox.classList.add("hide");
     rightBox.classList.remove("hide");
+    description.style.visibility='hidden';
+    header.style.visibility='visible';
 }
